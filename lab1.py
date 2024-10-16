94fdc5eebffc7a78086958f0af31e6febd8b301b
Wlad <283005@student.pwr.edu.pl>
2024-10-17 00:25:28 +0200

    Initial commit

---
+++ lab1.py
@@ -0,0 +1,53 @@
+import math
+
+def main():
+    while True:
+
+        print ("Please enter 3 coefficients of quadratic equation: ")
+
+        a = input()
+        b = input()
+        c = input()
+
+        try:
+            a = float(a)
+            b = float(b)
+            c = float(c)
+        except ValueError:
+            print("One of the provided values are not numbers")
+            if(not should_continue()):
+                print("Bye!")
+                break
+            continue
+
+        d = calculate_delta(a, b, c)
+
+        if (d < 0):
+            print("The equation doesn't have a solution")
+        elif (d == 0):
+            x = -b / (2 * a)
+            print ("The equation has one solution: ", x)
+        else:
+            x1, x2 = calculate_roots(a, b, d)
+            print (f"The equation has two solutions: x1 = {x1}, x2 = {x2}")
+        
+        if(not should_continue()):
+            print("Bye!")
+            break
+
+def calculate_delta(a: float, b: float, c: float) -> float:
+    delta = math.pow(b, 2) - 4 * a * c
+    return delta
+def calculate_roots(a: float, b: float, delta: float) -> tuple:
+    x1 = (-b + math.sqrt(delta)) / (2 * a)
+    x2 = (-b - math.sqrt(delta)) / (2 * a)
+    return x1, x2
+
+def should_continue() -> bool:
+    print("Do you want to continue ? y/n: ")
+    if(input() == "n"):
+        return False
+    
+    return True
+        
+main()
